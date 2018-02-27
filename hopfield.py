bug with sign

hopfield.py
@@ -8,7 +8,7 @@ def weights(X):
     return np.array(W)
 
 def sign(x):
-    if x > 0: return 1
+    if x >= 0: return 1
     if x < 0: return -1
     return 0
 